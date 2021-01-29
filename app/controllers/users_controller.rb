ece40d24d2b025afec8e5d8218412aab81a4b69a
gruidae <gruidae_2012101151@tutanota.com>
2021-01-29 16:56:46 +0000

    edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if !!@user.authenticate(params[:current_password])
+        
+        #@user.update(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to root_path
+        
+    
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                   :password_confirmation,:current_password )
     end
     
 end
