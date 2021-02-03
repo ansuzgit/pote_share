79c239cca6570f7b1d28071a128ae1055ddd44c3
gruidae <gruidae_2012101151@tutanota.com>
2021-02-03 05:15:54 +0000

    restart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_081042) do
+ActiveRecord::Schema.define(version: 2021_01_12_003510) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_01_27_081042) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
